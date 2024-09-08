003dd7b7a8262c912d27fa243f9dbed7c6502443
Artem Vyazemskiy <artvyz17@yandex.ru>
2024-09-08 16:50:22 +0300

    supercomputers: lab1

---
+++ 1/supercomputers/lab1/lab1.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <array>
+#include <random>
+#include <utility>
+#include <algorithm>
+#include <list>
+
+#include <omp.h>
+
+namespace
+{
+    bool isSimpleNumber(int number)
+    {
+        bool isSimple = true;
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                isSimple = false;
+                break;
+            }
+        }
+        return isSimple;
+    }
+}
+
+void example1()
+{
+    const int c_maxThreads = omp_get_max_threads();
+    std::cout << "Max threads: " << c_maxThreads << "\n";
+
+    int nThreads = c_maxThreads > 4 ? 4 : c_maxThreads;
+
+    #pragma omp parallel num_threads(nThreads)
+    {
+        int count = omp_get_num_threads();
+        int currentThread = omp_get_thread_num();
+
+        #pragma omp critical (cout)
+        {
+            std::cout << "Hello, OpenMP! I am " << currentThread << " of " << count << "\n";
+        }
+    }
+}
+
+void task5()
+{
+    constexpr size_t c_nNumbers = 100;
+    constexpr std::pair<int, int> c_range(1, 50);
+
+    std::array<int, c_nNumbers> numbers = {};
+
+    // Ќужно дл€ генерации случайных чисел
+    std::random_device dev;
+    std::mt19937 rng(dev());
+    std::uniform_int_distribution<std::mt19937::result_type> dist(c_range.first, c_range.second);
+    auto generateNumber = [&dist, &rng]() {
+        return dist(rng);
+    };
+
+    // √енерируем случайные натуральные числа и заполн€ем массив
+    std::generate(numbers.begin(), numbers.end(), generateNumber);
+
+    std::list<int> simpleNumbers;
+
+    /*
+        ƒинамическое планирование - т.к. дл€ разных чисел врем€ вычислений может сильно отличатьс€
+        shared - —овместное использование переменных между потоками
+    */
+    #pragma omp parallel for schedule(dynamic) shared(numbers, simpleNumbers)
+    for (int i = 0; i < numbers.size(); i++) {
+        const int number = numbers.at(i);
+        if (isSimpleNumber(number)) {
+            #pragma omp critical (push_back)
+            {
+                simpleNumbers.push_back(number);
+            }
+        }
+    }
+
+    std::cout << "numbers: ";
+    std::copy(numbers.cbegin(), numbers.cend(), std::ostream_iterator<int>(std::cout, ", "));
+    std::cout << "\nsimpleNumbers: ";
+    std::copy(simpleNumbers.cbegin(), simpleNumbers.cend(), std::ostream_iterator<int>(std::cout, ", "));
+
+}
+
+int main(int argc, char** argv)
+{
+    example1();
+    task5();
+
+    return 0;
+}
---
+++ 1/supercomputers/premake5.lua
@@ -0,0 +1,6 @@
+
+workspace "supercomputers"
+    architecture "x86_64"
+    configurations { "Debug", "Release" }
+
+include "lab1"
