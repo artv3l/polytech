39b60dae3ddf7b8530a9820f5dd97a4a83ca6f3f
Artem Vyazemskiy <artvyz17@yandex.ru>
2024-09-15 15:28:08 +0300

    lab6 comments

--- 1/supercomputers/lab6/prepare-files/main.cpp
+++ 1/supercomputers/lab6/prepare-files/main.cpp
@@ -14,17 +14,28 @@ void prepareFiles(const Matrix& a, const Matrix& b, size_t nProcesses, std::func
     for (size_t iProcess = 0; iProcess < nProcesses; iProcess++) {
         std::ofstream& file = getFile(iProcess);
 
+        // Максимальное кол-во строк в каком-либо из процессов. Нужно чтобы можно было передавать даные по кругу
+        // (Хотя для строк мб это и не нужно...)
         size_t aRowsBatchCount = (a.size() / nProcesses) + static_cast<size_t>(a.size() % nProcesses != 0);
+
+        // Кол-во строк для конкретно этого процесса. Столько строк будет записано в файл далее
         size_t aRowsCount = (a.size() / nProcesses) + static_cast<size_t>(iProcess < a.size() % nProcesses);
+
+        // a[0].size() - Кол-во элементов в строке
         file << aRowsBatchCount << " " << aRowsCount << " " << a[0].size() << "\n";
 
         size_t iRow = iProcess;
         while (iRow < a.size()) {
-            file << iRow << " ";
+            // Реальный индекс строки во всей результирующей матрице. Нужно для сбора результатов и объединения их в одну итоговую матрицу
+            file << iRow << " "; 
+
+            // Элементы строки
             std::copy(a[iRow].cbegin(), a[iRow].cend(), std::ostream_iterator<int>(file, " "));
             file << "\n";
             iRow += nProcesses;
         }
+
+        // Для столбцов аналогично, только не пишем кол-во элементов в столбце. Оно всегда равно кол-ву элементов в строке
 
         size_t bColumnsBatchCount = (b[0].size() / nProcesses) + static_cast<size_t>(b[0].size() % nProcesses != 0);
         size_t bColumnsCount = (b[0].size() / nProcesses) + static_cast<size_t>(iProcess < b[0].size() % nProcesses);
@@ -39,6 +50,7 @@ void prepareFiles(const Matrix& a, const Matrix& b, size_t nProcesses, std::func
             iColumn += nProcesses;
         }
 
+        // Размер итоговой матрицы
         file << a.size() << " " << b[0].size() << "\n";
     }
 }
