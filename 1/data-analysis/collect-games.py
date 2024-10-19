182aac581bc7725bab1b3e1dea8fda8de60fed34
Artem Vyazemskiy <artvyz17@yandex.ru>
2024-10-19 15:46:43 +0300

    Stream request, progress bar

--- 1/data-analysis/collect-games.py
+++ 1/data-analysis/collect-games.py
@@ -1,22 +1,31 @@
 import requests
 import configparser
-import ndjson
 import json
 import time
 import datetime
+import os
+from typing import Tuple, Callable
+import tqdm
 
 config_parser = configparser.ConfigParser()
 config_parser.read('config.ini')
 c_token = config_parser.get('default', 'token')
 c_username = config_parser.get('default', 'username')
 
+c_auth_headers = {'Authorization': f'Bearer {c_token}'}
+
 c_urls = {
-    'exportGames': 'https://lichess.org/api/games/user/{}'
+    'exportGames': 'https://lichess.org/api/games/user/{}',
+    'getUserPublicData': 'https://lichess.org/api/user/{}'
 }
 
-def export_games(username: str, token: str, count: int=None):
-    headers = {
-        'Authorization': f'Bearer {token}',
+def get_user_games_count(username: str, token: str):
+    headers = c_auth_headers
+    responce = requests.get(c_urls['getUserPublicData'].format(username), headers=headers)
+    return responce.json()['count']['all']
+
+def export_games(username: str, token: str, callback:Callable[[], None]=None, count: int=None):
+    headers = c_auth_headers | {
         'Accept': 'application/x-ndjson'
     }
     params = {
@@ -27,17 +36,37 @@ def export_games(username: str, token: str, count: int=None):
 
     if count is not None: params['max'] = count
     
-    responce = requests.get(c_urls['exportGames'].format(username), headers=headers, params=params)
-    return responce.json(cls=ndjson.Decoder)
+    result = []
+    responce = requests.get(c_urls['exportGames'].format(username), headers=headers, params=params, stream=True)
+    for chunk in responce.iter_lines():
+        result.append(json.loads(chunk))
+        if callback is not None: callback()
+    return result
 
-def export_games_to_file(filename: str, username: str, token: str, count: int=None) -> int:
-    games = export_games(username, token, count=count)
+def export_games_to_file(filename: str, username: str, token: str, count: int=None) -> Tuple[int, datetime.timedelta]:
+    games_count = get_user_games_count(username, token)
+    
+    with tqdm.tqdm(total=games_count) as pbar:
+        def callback():
+            pbar.update(1)
+        export_start_time = time.time()
+        games = export_games(username, token, count=count, callback=callback)
+        export_time = datetime.timedelta(seconds=(time.time() - export_start_time))
+
     with open(filename, 'w') as file:
-        json.dump(games, file, cls=ndjson.Encoder, indent=4)
-    return len(games)
+        json.dump(games, file, indent=4)
 
-export_start_time = time.time()
-games_count = export_games_to_file('games.json', c_username, c_token)
-export_time = datetime.timedelta(seconds=(time.time() - export_start_time))
+    return (len(games), export_time)
 
-print(f'Export {games_count} games in {export_time}')
+def main():
+    c_filename = f'games-{c_username}.json'
+
+    if os.path.exists(c_filename):
+        print(f'File \"{c_filename}\" already exists. Rewrite.')
+        os.remove(c_filename)
+
+    (exported_games_count, export_time) = export_games_to_file(c_filename, c_username, c_token)
+
+    #print(f'Export {exported_games_count} games in {export_time}.')
+
+main()
