80d3c972f3913efb5b8e9b9a4b713e8e126821df
Artem Vyazemskiy <artvyz17@yandex.ru>
2024-11-01 00:00:18 +0300

    Add plot_rating_diff

--- 1/data-analysis/analyze.py
+++ 1/data-analysis/analyze.py
@@ -6,10 +6,35 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def load_file(filename: str) -> pd.DataFrame:
-    games = pd.read_json(filename)
+def parse_players_info(row, username):
+    result = {}
+    (player_color, opponent_color) = ('white', 'black') if row['white']['user']['name'] == username else ('black', 'white')
+    result['rating'] = row[player_color]['rating']
+    result['ratingDiff'] = row[player_color]['ratingDiff']
+    result['opponent_rating'] = row[opponent_color]['rating']
+    result['opponent_ratingDiff'] = row[opponent_color]['ratingDiff']
+    return result
+
+def parse_game_result(row, username):
+    (player_color, opponent_color) = ('white', 'black') if row['players']['white']['user']['name'] == username else ('black', 'white')
+    if row['status'] == 'draw':
+        return 'draw'
+    elif row['winner'] == player_color:
+        return 'win'
+    else:
+        return 'lose'
+
+
+def load_file(filename: str, username: str) -> pd.DataFrame:
+    games = filter_type(pd.read_json(filename))
     games['createdAt'] = pd.to_datetime(games['createdAt'], unit='ms')
     games['lastMoveAt'] = pd.to_datetime(games['lastMoveAt'], unit='ms')
+
+    ppi = lambda x: pd.Series(parse_players_info(x, username))
+    games[['rating', 'ratingDiff', 'opponent_rating', 'opponent_ratingDiff']] = games['players'].apply(ppi)
+
+    games['game_result'] = games.apply(lambda x: parse_game_result(x, username), axis=1)
+
     return games
 
 def filter_type(games: pd.DataFrame) -> pd.DataFrame:
@@ -70,6 +95,30 @@ def plot_rating(games, username):
     plt.grid()
     plt.show()
 
+def plot_rating_diff(games: pd.DataFrame, merge_value: int):
+    games = games.copy()
+
+    def get_points(game_result):
+        if game_result == 'draw': return 0
+        elif game_result == 'win': return 1
+        else: return -1
+
+    games['rating_delta'] = games['rating'] - games['opponent_rating']
+    games['points'] = games['game_result'].apply(lambda x: get_points(x))
+
+    games = games.loc[abs(games['rating_delta']) < 300]
+
+    min_delta, max_delta = games['rating_delta'].min(), games['rating_delta'].max()
+    
+    bins = [i for i in range(min_delta, max_delta, merge_value)]
+    labels = [(i+(merge_value/2)) for i in range(min_delta, max_delta-merge_value, merge_value)]
+
+    games['rating_delta_bins'] = pd.cut(games['rating_delta'], bins=bins, labels=labels)
+    stats = games.groupby('rating_delta_bins', observed=False)['points'].sum()
+
+    plt.plot(stats.index.tolist(), stats.values)
+    plt.show()
+
 
 def main():
     config_parser = configparser.ConfigParser()
@@ -78,11 +127,10 @@ def main():
 
     c_filename = f'games-{c_username}.json'
 
-    games = filter_type(load_file(c_filename))
-
+    games = load_file(c_filename, c_username)
     games = filter_date(games, '2020-09-25', '2024-09-30')
 
-    plot_time(games)
-    plot_rating(games, c_username)
+    merge_value = 20 # 5 .. 25
+    plot_rating_diff(games, merge_value)
 
 main()
