80b0a7e41af08fbdb134f0199f15a0be2695b99d
Artem Vyazemskiy <artvyz17@yandex.ru>
2025-06-03 19:28:34 +0300

    sp1 util

---
+++ 2/sp/premake5.lua
@@ -0,0 +1,12 @@
+workspace "sp"
+    architecture "x86_64"
+    configurations { "Debug", "Release" }
+    startproject "task1"
+
+project "task1"
+    kind "ConsoleApp"
+    language "C++"
+    cppdialect "C++17"
+
+    files { "komppl.c" }
+    
