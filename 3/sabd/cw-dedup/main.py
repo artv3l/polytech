8c9c8dd071d7eac4249c7555f45eab60240f9eb3
Artem Vyazemskiy <artvyz17@yandex.ru>
2025-10-19 13:47:43 +0300

    sabd cw: plots

--- 3/sabd/cw-dedup/main.py
+++ 3/sabd/cw-dedup/main.py
@@ -4,26 +4,42 @@ import params
 import pathlib
 
 
-if __name__ == "__main__":
+def stats_one(file: pathlib.Path, storage: dedup.Storage):
+    print(file.name)
+    with (
+        open(files[0], "rb") as file,
+        open(params.fmt_ref(files[0]), "wb") as ref_file,
+    ):
+        print(stats.store_operation_stats(file, params.chunk_size, ref_file, storage))
+
+    with (
+        open(params.fmt_ref(files[0]), "rb") as ref_file,
+        open(params.fmt_deref(files[0]), "wb") as out_file,
+    ):
+        print(stats.get_operation_stats(ref_file, out_file, storage))
+    print()
+
+
+if __name__ == "__main__1":
     folder = pathlib.Path.cwd() / pathlib.Path("test")
 
     files = [
-        folder / pathlib.Path("test.txt")
+        # folder / pathlib.Path("01. Barricades.flac"),
+        # folder / pathlib.Path("Fedora-KDE-Desktop-Live-42-1.1.x86_64.iso"),
+        folder / pathlib.Path("img.bmp"),
+        folder / pathlib.Path("img2.bmp"),
     ]
 
     with dedup.Storage(
         "mongodb://root:root@localhost:27017/", "sabd-cw", "refs", "test/data.bin"
     ) as storage:
-        with (
-            open(files[0], "rb") as file,
-            open(params.fmt_ref(files[0]), "wb") as ref_file,
-        ):
-            print(stats.store_operation_stats(file, params.chunk_size, ref_file, storage))
+        #for file in files:
+        #    stats_one(file, storage)
 
-        with (
-            open(params.fmt_ref(files[0]), "rb") as ref_file,
-            open(params.fmt_deref(files[0]), "wb") as out_file,
-        ):
-            print(stats.get_operation_stats(ref_file, out_file, storage))
+        stats.calc_chunk_size(storage, files, stats.CalcChunkSizeParams(begin=2048, end=2048 * 10, count=30))
 
-        stats.graphs(storage, files, stats.CalcChunkSizeParams())
+if __name__ == "__main__":
+    folder = pathlib.Path.cwd() / pathlib.Path("test")
+    for file in folder.iterdir():
+        if file.suffix == ".json":
+            jsons = stats.make_graph(file)
--- 3/sabd/cw-dedup/stats.py
+++ 3/sabd/cw-dedup/stats.py
@@ -6,6 +6,9 @@ import params
 import pathlib
 import pydantic
 import json
+import matplotlib.pyplot as plt
+import matplotlib.figure
+import functools
 
 
 def get_collection_size(collection: pymongo.collection.Collection) -> int:
@@ -84,15 +87,16 @@ class StoreStats(BaseStats):
 
     def __str__(self):
         return f"""Store:
-    Total memory diff: {self.mem_diff / 2**20} MB
-    Dedup ratio: {self.dedup_ratio}
-    Stored {self.user_stats.file_size / 2**20} MB in {self.storage_stats_diff.time_seconds} s, {self.speed / 2**20} MB/s"""
+    Total memory diff: {self.mem_diff / 2**20:.2f} MB
+    User memory diff: {self.user_stats.file_size / 2**20:.2f} MB -> {self.user_stats.ref_file_size / 2**10:.2f} KB
+    Dedup ratio: {self.dedup_ratio * 100:.2f} %
+    Stored {self.user_stats.file_size / 2**20:.2f} MB in {self.storage_stats_diff.time_seconds:.2f} s, {self.speed / 2**20:.2f} MB/s"""
 
 
 class GetStats(BaseStats):
     def __str__(self):
         return f"""Get:
-    Get {self.user_stats.file_size / 2**20} MB in {self.storage_stats_diff.time_seconds} s, {self.speed / 2**20} MB/s"""
+    Get {self.user_stats.file_size / 2**20:.2f} MB in {self.storage_stats_diff.time_seconds:.2f} s, {self.speed / 2**20:.2f} MB/s"""
 
 
 def get_storage_stats(storage: dedup.Storage) -> StorageStats:
@@ -134,68 +138,189 @@ def get_operation_stats(
     )
 
 
-class CalcChunkSizeParams:
-    begin_part: float = 0.05 / 100
-    end_part: float = 0.5 / 100
-    count: int = 20
+class CalcChunkSizeParams(pydantic.BaseModel):
+    begin: int
+    end: int
+    count: int = 50
 
 
-class CalcChunkSizeResult(pydantic.BaseModel):
-    chunk_size: int
+class FileInfo(pydantic.BaseModel):
+    file_name: str
     file_size: int
     store_stats: StoreStats
     get_stats: GetStats
+
+
+CalcChunkSizeResult = typing.Dict[int, typing.List[FileInfo]]
 
 
 def fmt_stat(filename: pathlib.Path) -> pathlib.Path:
     return filename.with_name(filename.name + ".json")
 
 
-def graphs(
+def calc_chunk_size(
     storage: dedup.Storage,
     files: typing.List[pathlib.Path],
     calc_params: CalcChunkSizeParams,
 ):
-    for file in files:
-        if file.is_file():
+    result: CalcChunkSizeResult = {}
+
+    step = int((calc_params.end - calc_params.begin) / calc_params.count)
+    for chunk_size in range(calc_params.begin, calc_params.end, step):
+        storage.clean()
+        result[chunk_size] = []
+
+        for file in files:
+            if not file.is_file():
+                return
             file_size = get_size(file)
             if file_size == 0:
                 continue
 
             file_name = file.relative_to(pathlib.Path.cwd())
 
-            storage.clean()
+            ref_filepath = params.fmt_ref(file_name)
+            out_filepath = params.fmt_deref(file_name)
+            with (
+                open(file_name, "rb") as file_s,
+                open(ref_filepath, "wb") as ref_file,
+            ):
+                store_stats = store_operation_stats(
+                    file_s, chunk_size, ref_file, storage
+                )
+            with (
+                open(ref_filepath, "rb") as ref_file,
+                open(out_filepath, "wb") as out_file,
+            ):
+                get_stats = get_operation_stats(ref_file, out_file, storage)
 
-            beg = int(file_size * calc_params.begin_part)
-            end = int(file_size * calc_params.end_part)
-            step = int((end - beg) / calc_params.count)
+            result[chunk_size].append(
+                FileInfo(
+                    file_name=file_name.name,
+                    file_size=file_size,
+                    store_stats=store_stats,
+                    get_stats=get_stats,
+                )
+            )
 
-            result = []
+            ref_filepath.unlink()
+            out_filepath.unlink()
 
-            for chunk_size in range(beg, end, step):
-                ref_filepath = params.fmt_ref(file_name)
-                out_filepath = params.fmt_deref(file_name)
-                with (
-                    open(file_name, "rb") as file_s,
-                    open(ref_filepath, "w+b") as ref_file,
-                    open(out_filepath, "wb") as out_file,
-                ):
-                    store_stats = store_operation_stats(
-                        file_s, chunk_size, ref_file, storage
-                    )
-                    get_stats = get_operation_stats(ref_file, out_file, storage)
+    result_json = {
+        chunk_size: [file_info.model_dump() for file_info in file_infos]
+        for chunk_size, file_infos in result.items()
+    }
+    json_filename = fmt_stat(file_name)
+    with open(json_filename, "w") as stat_file:
+        json.dump(result_json, stat_file, indent=4)
 
-                    result.append(
-                        CalcChunkSizeResult(
-                            chunk_size=chunk_size,
-                            file_size=file_size,
-                            store_stats=store_stats,
-                            get_stats=get_stats,
-                        )
-                    )
-                ref_filepath.unlink()
-                out_filepath.unlink()
 
-            result_json = [res.model_dump() for res in result]
-            with open(fmt_stat(file_name), "w") as stat_file:
-                json.dump(result_json, stat_file, indent=4)
+def extract_values(result: CalcChunkSizeResult, extract_func):
+    values = {}
+    for file_infos in result.values():
+        for file_info in file_infos:
+            if file_info.file_name not in values:
+                values[file_info.file_name] = []
+            values[file_info.file_name].append(extract_func(file_info))
+    return values
+
+
+PltFigure = matplotlib.figure.Figure
+CalcFunc = typing.Callable[
+    [typing.Any], typing.Any
+]  # Подготавливает данные для построения графика
+PlotFunc = typing.Callable[
+    [typing.Any], typing.List[PltFigure]
+]  # Строит график по подготовленным данным
+OutFunc = typing.Callable[
+    [PltFigure], None
+]  # Обработка графика: вывод на экран, сохранение в файл
+bind = functools.partial
+
+
+def process_data(
+    data, calc_func: CalcFunc, plot_funcs: typing.List[typing.Tuple[PlotFunc, OutFunc]]
+):
+    calc_result = calc_func(data) if (calc_func is not None) else data
+    for plot_func, out_func in plot_funcs:
+        figure = plot_func(calc_result)
+        out_func(figure)
+        plt.close(figure)
+
+
+def save_to_file(figure: PltFigure, title: str):
+    figure.tight_layout()
+    path = "plots"
+    pathlib.Path(path).mkdir(parents=True, exist_ok=True)
+    figure.savefig(f"{path}/{title}.png", dpi=300)
+
+
+def binded_save(title: str):
+    return functools.partial(save_to_file, title=title)
+
+
+def show(figure: PltFigure):
+    figure.show()
+    plt.show()
+
+
+def plot_values_by_chunk_size(data, extract_func, ylabel) -> PltFigure:
+    fig, ax = plt.subplots(nrows=1, ncols=1)
+
+    chunk_sizes = [int(chunk_size) / 2**10 for chunk_size in data.keys()]
+    store_speeds = extract_values(data, extract_func)
+    for file_name, vals in store_speeds.items():
+        ax.plot(chunk_sizes, vals, label=file_name)
+    ax.legend()
+    ax.set_xlabel("chunk_size, KB")
+    ax.set_ylabel(ylabel)
+    return fig
+
+
+def make_graph(json_filename: pathlib.Path):
+    with open(json_filename, "r") as stat_file:
+        data = json.load(stat_file)
+    data = {
+        chunk_size: [FileInfo(**file_info) for file_info in file_infos]
+        for chunk_size, file_infos in data.items()
+    }
+
+    process_data(
+        data,
+        None,
+        [
+            (
+                bind(
+                    plot_values_by_chunk_size,
+                    extract_func=lambda file_info: file_info.store_stats.speed / 2**20,
+                    ylabel="store_speed, MB/s",
+                ),
+                binded_save("store_speeds"),
+            ),
+            (
+                bind(
+                    plot_values_by_chunk_size,
+                    extract_func=lambda file_info: file_info.store_stats.dedup_ratio
+                    * 100,
+                    ylabel="dedup_ratio, %",
+                ),
+                binded_save("dedup_ratios"),
+            ),
+            (
+                bind(
+                    plot_values_by_chunk_size,
+                    extract_func=lambda file_info: file_info.get_stats.speed / 2**20,
+                    ylabel="get_speed, MB/s",
+                ),
+                binded_save("get_speeds"),
+            ),
+            (
+                bind(
+                    plot_values_by_chunk_size,
+                    extract_func=lambda file_info: file_info.store_stats.mem_diff / 2**20,
+                    ylabel="mem_diff, MB",
+                ),
+                binded_save("mem_diffs"),
+            ),
+        ],
+    )
